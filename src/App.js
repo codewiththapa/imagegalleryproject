d863603b619e601a0aa8cbdcaca359c009089245
codewiththapa <thapasuman2021@gmail.com>
2024-09-22 00:27:06 +0400

    ImageGalleryProject

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 import axios from 'axios';
 import { ACCESS_KEY } from './config/constants';
 
 const App = () => {
+ const [imageList,setImageList]= useState([]);
+ const [tempImageList,setTempImageList] = useState([]);
+ const [isLoading,setIsLoading] = useState (true);
   useEffect(()=>{
-   axios.get('https://api.unsplash.com/photos/?client_id=${ACCESS_KEY}&per_page=30');
+    document.title = "Image Gallery App"
+   axios.get(`https://api.unsplash.com/photos/?client_id=${ACCESS_KEY}&per_page=30`).then(response=>{
+    setImageList(response.data);
+    setTempImageList(response.data);
+    setIsLoading(false);
+   });
   },[]);
+  const searchImage = (query) =>{
+    if(query===""){
+      setImageList(tempImageList);
+    } else{
+   const filteredImageList = imageList.filter(image=>{
+      image.alt_description = image.alt_description === null ? "React" : 
+      image.alt_description;
+      return image.alt_description.includes(query);
+    });
+     setImageList(filteredImageList);
+  }
+  };
   return (
     <div>
-      Hi
+      <center>
+      <input type="text" 
+      style={{height:"40px",width:"50%",borderRadius:'6px'}} 
+      placeholder='Search images..'
+      onChange={(e)=>searchImage(e.target.value)}/>
+      </center>
+     
+     {/*//image cointainer*/}
+      <div style={{display:"flex",flexWrap:"wrap",justifyContent:"center"}}>
+     {imageList.length>0?imageList.map((image)=>{
+      return (
+      <div key={image.id} 
+      style={{padding:"20px", textAlign:"center"}}>
+        <img 
+        src={image.urls.regular} 
+        style={{height:"250px",width:"250px",objectFit:"cover"}}
+        alt={image.alt_description}
+        />
+        <br />
+      {image.alt_description ? image.alt_description.substring(0,20)
+      :"React"}
+        </div>
+     );
+     })
+     :isLoading?'Loading...'
+     :'No images found !!'} 
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
